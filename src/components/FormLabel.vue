b1630c9268aa5239a6c20a0e3580b26e1ee1893a
kmsheng <kmsh3ng@gmail.com>
2022-11-29 20:24:49 +0800

    update FormLabel

--- src/components/FormLabel.vue
+++ src/components/FormLabel.vue
@@ -1,14 +1,14 @@
 <script setup>
-  defineProps({
-    forId: {
-      type: String,
-      default: ''
-    }
-  })
+defineProps({
+  forId: {
+    type: String,
+    default: ''
+  }
+})
 </script>
 
 <template>
-  <label for="forId" class="text-[#8e8f94] font-medium text-xs">
+  <label for="forId" class="text-[#8e8f94] font-semibold text-xs">
     <slot />
   </label>
 </template>
