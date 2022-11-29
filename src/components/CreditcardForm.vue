2ae65c44977c0a7b9a72b118a69f549239072822
kmsheng <kmsh3ng@gmail.com>
2022-11-29 19:59:29 +0800

    add drop shadow

--- src/components/CreditcardForm.vue
+++ src/components/CreditcardForm.vue
@@ -4,7 +4,7 @@ import FormLabel from './FormLabel.vue'
 </script>
 
 <template>
-  <div class="creditcard-form w-[480px] px-12 py-8 rounded-lg">
+  <div class="creditcard-form w-[440px] px-12 py-8 rounded-lg drop-shadow-2xl">
     <div class="text-white font-medium text-2xl mb-12">Card Details</div>
     <div>
       <form-label>Card Type</form-label>
