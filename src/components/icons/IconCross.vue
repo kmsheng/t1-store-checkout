96fcc6dd3ccf9b691c8d92bf28684f24a7911e09
kmsheng <kmsh3ng@gmail.com>
2022-11-29 14:40:26 +0800

    add IconCross.vue

---
+++ src/components/icons/IconCross.vue
@@ -0,0 +1,6 @@
+<template>
+<svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <line x1="6.04877" y1="15.6234" x2="15.2412" y2="6.43106" stroke="#393940" stroke-width="2" stroke-linecap="round"/>
+  <line x1="15.2412" y1="15.5689" x2="6.04879" y2="6.37652" stroke="#393940" stroke-width="2" stroke-linecap="round"/>
+</svg>
+</template>
