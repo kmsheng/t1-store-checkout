0cb7c4d2f7d6ff875b57954ae52370085cd992f4
kmsheng <kmsh3ng@gmail.com>
2022-11-29 15:21:29 +0800

    update CheckoutPage.vue

--- src/pages/CheckoutPage.vue
+++ src/pages/CheckoutPage.vue
@@ -1,21 +1,20 @@
 <script setup>
+import CartList from '../components/CartList.vue'
+import CreditcardForm from '../components/CreditcardForm.vue'
 </script>
 
 <template>
-  <div>
-    <div>
-      <div>Shopping Cart</div>
-      <div>
+  <div class="flex justify-center bg-[#f6f6f6] text-[#393940]">
+    <div class="container flex py-14">
+      <div class="grow pl-6 pr-14">
+        <div class="text-2xl font-bold">Shopping Cart</div>
+        <cart-list />
+        <div class="flex">
+          <a>Continue Shopping</a>
+          <div>Subtotal: $24.90</div>
+        </div>
       </div>
-      <div>
-        <a>Continue Shopping</a>
-        <div>Subtotal: $24.90</div>
-      </div>
-    </div>
-    <div>
-      <div>Card Details</div>
-      <div>
-      </div>
+      <creditcard-form />
     </div>
   </div>
 </template>
