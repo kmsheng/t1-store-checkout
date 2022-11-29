2034be607beb63a56d7cd6bae14d78b38769f5ab
kmsheng <kmsh3ng@gmail.com>
2022-11-29 15:20:37 +0800

    add QtyInput.vue

---
+++ src/components/QtyInput.vue
@@ -0,0 +1,25 @@
+<script setup>
+defineProps({
+  qty: {
+    type: Number,
+    required: true
+  }
+})
+
+const emit = defineEmits(['change'])
+</script>
+
+<template>
+  <div class="flex">
+    <button class="btn-qty" @click="emit('change', qty - 1)">-</button>
+    <input
+      class="w-14 rounded border border-[#cdcdd2] px-2 py-1 text-center"
+      type="number"
+      :value="qty"
+    />
+    <button class="btn-qty" @click="emit('change', qty + 1)">+</button>
+  </div>
+</template>
+
+<style lang="css" scoped>
+</style>
