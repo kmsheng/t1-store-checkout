90d061abb6f783667f57e1f45637e4ce54dd5318
kmsheng <kmsh3ng@gmail.com>
2022-11-29 11:03:39 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup>
+import CheckoutPage from './pages/CheckoutPage.vue'
+</script>
+
+<template>
+  <CheckoutPage />
+</template>
---
+++ src/pages/CheckoutPage.vue
@@ -0,0 +1,21 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    <div>
+      <div>Shopping Cart</div>
+      <div>
+      </div>
+      <div>
+        <a>Continue Shopping</a>
+        <div>Subtotal: $24.90</div>
+      </div>
+    </div>
+    <div>
+      <div>Card Details</div>
+      <div>
+      </div>
+    </div>
+  </div>
+</template>
