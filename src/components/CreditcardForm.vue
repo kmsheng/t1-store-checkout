1f15d8dc8d45144e6489a06cf4b54452f5531ec2
kmsheng <kmsh3ng@gmail.com>
2022-11-29 15:20:45 +0800

    add CreditcardForm.vue

---
+++ src/components/CreditcardForm.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+  <div class="creditcard-form w-[480px] px-12 py-8 rounded-lg">
+    <div class="text-white font-medium text-2xl">Card Details</div>
+    <div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.creditcard-form {
+  background: linear-gradient(45deg, #2c2d31, #42434c);
+}
+</style>
