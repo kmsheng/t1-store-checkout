8aa8100ffc7b8d46baf650e28b480c2c4e5affe2
kmsheng <kmsh3ng@gmail.com>
2022-11-29 20:24:57 +0800

    update CreditcardForm

--- src/components/CreditcardForm.vue
+++ src/components/CreditcardForm.vue
@@ -1,6 +1,14 @@
 <script setup>
 import FakeCreditcard from './FakeCreditcard.vue'
 import FormLabel from './FormLabel.vue'
+import LabelInput from './LabelInput.vue'
+
+defineProps({
+  form: {
+    type: Object,
+    required: true
+  }
+})
 </script>
 
 <template>
@@ -12,5 +20,7 @@ import FormLabel from './FormLabel.vue'
         <fake-creditcard class="-translate-x-[90px]" />
       </div>
     </div>
+    <label-input class="mt-6" label="Name on Card" v-model="form.cardName" />
+    <label-input class="mt-6" label="Card Number" v-model="form.cardNumber" />
   </div>
 </template>
