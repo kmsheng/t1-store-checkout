cc469f315b45753da94a435e40f7fe797088eea0
kmsheng <kmsh3ng@gmail.com>
2022-11-29 20:24:34 +0800

    add LabelInput.vue

---
+++ src/components/LabelInput.vue
@@ -0,0 +1,28 @@
+<script setup>
+import FormLabel from './FormLabel.vue'
+
+const emit = defineEmits(['change'])
+
+defineProps({
+  label: {
+    type: String,
+    required: true
+  },
+  modelValue: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div>
+    <form-label class="block mb-1">{{ label }}</form-label>
+    <input
+      type="text"
+      class="bg-transparent text-[#edeef5] border-b border-gray-500 py-3 w-full"
+      :value="modelValue"
+      @change="emit('change', $event.target.value)"
+    />
+  </div>
+</template>
