c94080c38634aab717c489557f557a9420eaeaec
kmsheng <kmsh3ng@gmail.com>
2022-11-30 13:41:30 +0800

    add base for production site

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/t1-store-checkout/',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
