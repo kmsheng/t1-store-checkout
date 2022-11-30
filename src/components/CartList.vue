0a866e7e73951e7a0e8ec9f4c6a4144b431bdc0f
kmsheng <kmsh3ng@gmail.com>
2022-11-30 12:03:53 +0800

    handle responsive class

--- src/components/CartList.vue
+++ src/components/CartList.vue
@@ -17,22 +17,29 @@ defineProps({
     <div
       v-for="item in items"
       :key="item.id"
-      class="flex place-content-between items-center border-b border-[#e5e5e7]">
-      <img class="w-44 h-44" :src="item.imgSrc" :alt="item.alt" />
-      <div class="px-8 grow">
-        <div class="font-bold" v-text="item.name" />
-        <div class="text-[#c2c3c3] text-sm">#{{ item.id }}</div>
+      class="flex flex-col xl:flex-row xl:place-content-between border-b border-[#e5e5e7]">
+
+      <div class="flex items-center">
+        <img class="w-28 h-28 md:w-44 md:h-44" :src="item.imgSrc" :alt="item.alt" />
+        <div class="px-8 grow">
+          <div class="font-bold" v-text="item.name" />
+          <div class="text-[#c2c3c3] text-sm">#{{ item.id }}</div>
+        </div>
       </div>
-      <qty-input class="px-4" :qty="item.qty" @change="qty => emit('qtyChange', item.id, qty)" />
-      <div class="font-bold px-4 whitespace-nowrap">
-        <span class="inline-block scale-75 translate-x-0.5 -translate-y-1" v-text="'$'" />
-        {{ (item.price * item.qty).toFixed(2) }}
+
+      <div class="flex items-center place-content-between grow pb-4 sm:grow-0">
+        <qty-input class="px-0 lg:px-4" :qty="item.qty" @change="qty => emit('qtyChange', item.id, qty)" />
+        <div class="font-bold px-4 whitespace-nowrap grow">
+          <span class="inline-block scale-75 translate-x-0.5 -translate-y-1" v-text="'$'" />
+          {{ (item.price * item.qty).toFixed(2) }}
+        </div>
+        <button
+          class="inline-block px-1.5 py-1 hover:bg-[#e7e7e7] transition-colors duration-300"
+          @click="emit('delete', item)">
+          <icon-cross />
+        </button>
       </div>
-      <button
-        class="inline-block px-1.5 py-1 hover:bg-[#e7e7e7] transition-colors duration-300"
-        @click="emit('delete', item)">
-        <icon-cross />
-      </button>
+
     </div>
   </div>
 </template>
--- src/pages/CheckoutPage.vue
+++ src/pages/CheckoutPage.vue
@@ -26,21 +26,21 @@ const setForm = (prop, value) => {
 
 <template>
   <div class="flex justify-center bg-[#f6f6f6] text-[#393940]">
-    <div class="container flex py-12">
-      <div class="grow pr-14">
-        <div class="text-2xl font-bold mb-14">Shopping Cart</div>
+    <div class="container flex flex-col lg:flex-row py-4 sm:py-12 mx-4 md:mx-auto">
+      <div class="grow p-4 md:pr-14 md:pl-0 md:py-0">
+        <div class="text-2xl font-bold mb-4 sm:mb-14">Shopping Cart</div>
         <div v-if="product.items.length === 0">Your cart is empty.</div>
         <cart-list v-else
           :items="product.items"
           @qty-change="product.setQty"
           @delete="target => product.deleteItem(target.id)"
         />
-        <div class="flex place-content-between py-8">
-          <a class="flex items-center text-[#3777da] font-bold">
+        <div class="flex flex-col md:flex-row items-end place-content-between py-8">
+          <a class="flex order-2 md:order-1 mt-4 md:mt-auto  items-center text-[#3777da] font-bold">
             <icon-arrow-left class="mr-3" />
             Continue Shopping
           </a>
-          <div class="text-[#c2c3c3] font-semibold">
+          <div class="text-[#c2c3c3] font-semibold order-1 md:order-2">
             Subtotal:
             <strong class="font-semibold text-2xl text-[#393940]">${{ product.subtotal.toFixed(2) }}</strong>
           </div>
