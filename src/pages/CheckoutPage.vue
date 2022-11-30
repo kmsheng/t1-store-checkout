aa4d14e6f407751ca116d6213c9d96b43633bc67
kmsheng <kmsh3ng@gmail.com>
2022-11-30 11:06:56 +0800

    apply pinia useProductStore to CheckoutPage

--- src/pages/CheckoutPage.vue
+++ src/pages/CheckoutPage.vue
@@ -3,50 +3,9 @@ import { computed, reactive } from 'vue'
 import CartList from '../components/CartList.vue'
 import CreditcardForm from '../components/CreditcardForm.vue'
 import IconArrowLeft from '../components/icons/IconArrowLeft.vue'
-import food1 from '../assets/food1.jpg'
-import food2 from '../assets/food2.jpg'
-import food3 from '../assets/food3.jpg'
+import { useProductStore } from '../stores/product.js'
 
-const items = [
-  {
-    id: '4231648',
-    name: 'Chicken momo',
-    qty: 1,
-    price: 10.5,
-    imgSrc: food1,
-    alt: 'Three chicken momos on a plate.'
-  },
-  {
-    id: '4231649',
-    name: 'Spicy Mexican potatoes',
-    qty: 1,
-    price: 8.5,
-    imgSrc: food2,
-    alt: 'Three chicken momos on a plate.'
-  },
-  {
-    id: '4231650',
-    name: 'Breakfast',
-    qty: 1,
-    price: 5.9,
-    imgSrc: food3,
-    alt: 'Three chicken momos on a plate.'
-  },
-]
-
-const state = reactive({ items })
-
-const setQty = (item, qty) => {
-  if (qty >= 1) {
-    item.qty = qty
-  }
-}
-
-const deleteItem = target => {
-  state.items = state.items.filter(item => item.id !== target.id)
-}
-
-const subtotal = computed(() => state.items.reduce((s, item) => s + (item.price * item.qty), 0))
+const product = useProductStore()
 
 const form = reactive({
   cardName: 'Giga Tamarashvili',
@@ -70,8 +29,12 @@ const setForm = (prop, value) => {
     <div class="container flex py-12">
       <div class="grow pr-14">
         <div class="text-2xl font-bold mb-14">Shopping Cart</div>
-        <div v-if="state.items.length === 0">Your cart is empty.</div>
-        <cart-list v-else :items="state.items" @qty-change="setQty" @delete="deleteItem" />
+        <div v-if="product.items.length === 0">Your cart is empty.</div>
+        <cart-list v-else
+          :items="product.items"
+          @qty-change="product.setQty"
+          @delete="target => product.deleteItem(target.id)"
+        />
         <div class="flex place-content-between py-8">
           <a class="flex items-center text-[#3777da] font-bold">
             <icon-arrow-left class="mr-3" />
@@ -79,7 +42,7 @@ const setForm = (prop, value) => {
           </a>
           <div class="text-[#c2c3c3] font-semibold">
             Subtotal:
-            <strong class="font-semibold text-2xl text-[#393940]">${{ subtotal.toFixed(2) }}</strong>
+            <strong class="font-semibold text-2xl text-[#393940]">${{ product.subtotal.toFixed(2) }}</strong>
           </div>
         </div>
       </div>
