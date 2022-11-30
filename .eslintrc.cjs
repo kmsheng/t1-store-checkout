e4ba496464363adf04d812dda85f42764009f0cd
kmsheng <kmsh3ng@gmail.com>
2022-11-30 11:08:48 +0800

    add env prop to .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,10 @@
 /* eslint-env node */
 module.exports = {
   root: true,
+  env: {
+    browser: true,
+    node: true
+  },
   'extends': [
     'plugin:vue/vue3-essential',
     'eslint:recommended'
