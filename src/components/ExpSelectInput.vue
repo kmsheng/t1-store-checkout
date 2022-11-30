ec9d8629641a2c0c5d7b3f36573d5d6035944e96
kmsheng <kmsh3ng@gmail.com>
2022-11-30 09:56:38 +0800

    add ExpSelectInput.vue

---
+++ src/components/ExpSelectInput.vue
@@ -0,0 +1,34 @@
+<script setup>
+import FormLabel from './FormLabel.vue'
+
+const emit = defineEmits(['change'])
+
+defineProps({
+  year: {
+    type: String,
+    required: true
+  },
+  month: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div>
+    <form-label class="block mb-1">Expiration date</form-label>
+    <div class="flex">
+      <select :value="month" class="select mr-4">
+        <option value="">mm</option>
+      </select>
+      <input
+        type="text"
+        class="input w-auto"
+        placeholder="yyyy"
+        :value="year"
+        @input="emit('change', $event.target.value)"
+      />
+    </div>
+  </div>
+</template>
