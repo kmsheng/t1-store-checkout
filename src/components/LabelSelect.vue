bee8b1b5c8aa84b7d038464e9a12bbcce884482a
kmsheng <kmsh3ng@gmail.com>
2022-11-30 09:56:45 +0800

    add LabelSelect.vue

---
+++ src/components/LabelSelect.vue
@@ -0,0 +1,25 @@
+<script setup>
+import FormLabel from './FormLabel.vue'
+
+const emit = defineEmits(['change'])
+
+defineProps({
+  label: {
+    type: String,
+    required: true
+  },
+  modelValue: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div>
+    <form-label class="block mb-1">{{ label }}</form-label>
+    <select :value="modelValue" class="select">
+      <option value="">mm</option>
+    </select>
+  </div>
+</template>
