b25710816de2b936ed057e601849cfb25644d277
kmsheng <kmsh3ng@gmail.com>
2022-11-30 12:47:23 +0800

    add IconMasterCard.vue

---
+++ src/components/icons/IconMasterCard.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <div class="flex relative">
+      <div class="w-10 h-10 bg-[#ec001b] rounded-full translate-x-2 absolute left-0" />
+      <div class="w-10 h-10 bg-[#ec001b] rounded-full translate-x-2 z-10 opacity-40" />
+      <div class="w-10 h-10 bg-[#f69f19] rounded-full -translate-x-2" />
+    </div>
+    <div class="text-white text-center text-xs">mastercard.</div>
+  </div>
+</template>
